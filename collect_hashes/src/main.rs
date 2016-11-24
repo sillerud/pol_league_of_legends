b5ecae1769ac866abaf3caac6c1d33b2bcddc174
Kevin Sillerud <kevin.sillerud@gmail.com>
2016-11-24 21:55:12 +0100

    Create a rust program to calculate hashes and generate a few variables and some minor cleanups

---
+++ collect_hashes/src/main.rs
@@ -0,0 +1,132 @@
+extern crate crypto;
+extern crate hyper;
+
+use std::io::{self, Read, Write};
+use std::path::Path;
+use std::fs;
+use std::fs::File;
+use std::collections::HashMap;
+//use std::slice::SliceConcatExt;
+
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+
+use hyper::Client;
+use hyper::status::StatusCode;
+
+#[derive(Hash, Eq, PartialEq, Clone)]
+struct LoLVersion {
+    region: &'static str,
+    version: &'static str,
+    description: &'static str
+}
+
+fn main() {
+    let versions = vec!(
+        LoLVersion {
+            version: "2016_11_10",
+            region: "EUW",
+            description: "EU West"
+        },
+        LoLVersion {
+            region: "NA",
+            version: "2016_05_13",
+            description: "North America"
+        },
+        LoLVersion {
+            region: "EUNE",
+            version: "2016_11_10",
+            description: "EU Nordic & East"
+        },
+        LoLVersion {
+            region: "OC1",
+            version: "2016_05_13",
+            description: "Oceania"
+        },
+        LoLVersion {
+            region: "RU",
+            version: "2016_05_13",
+            description: "Russia"
+        },
+        LoLVersion {
+            region: "LA1",
+            version: "2016_05_26",
+            description: "Latin America North"
+        },
+        LoLVersion {
+            region: "LA2",
+            version: "2016_05_27",
+            description: "Latin America South"
+        },
+        LoLVersion {
+            region: "BR",
+            version: "2016_05_13",
+            description: "Brazil"
+        });
+    let mut map = HashMap::new();
+    let mut regions_found: Vec<&'static str> = vec!();
+    let http_client = Client::new();
+    let mut hash = Md5::new();
+    let cache_folder = Path::new("filecache");
+    if !cache_folder.exists() {
+        fs::create_dir(cache_folder).expect("Failed to create installer cache folder");
+    }
+    for version in versions.clone() {
+        let download_url = get_url(&version);
+        println!("Calculating hash for url: {}", download_url);
+        // Check if file exists in cache
+        let executable = cache_folder.join(format!("LeagueofLegends_{}_Installer_{}.exe", version.region, version.version));
+        if !executable.exists() {
+            if !download(&http_client, &download_url, &executable) {
+                println!("Warning: {} returned 404", download_url);
+                continue;
+            }
+        }
+
+        let mut file = File::open(&executable).expect(format!("Could not find installer: {:?}", executable).as_ref());
+
+
+        let mut bytes = vec!();
+        file.read_to_end(&mut bytes).expect("Failed to read remote content");
+        hash.input(&bytes);
+        let hash_result = hash.result_str();
+        println!("{}", &hash_result);
+        map.insert(version, hash_result);
+        hash.reset();
+    }
+
+    let hashes_map: Vec<String> = map
+        .iter()
+        .map(|(version, hash)| format!("[\"{}\"]=\"{}\"", version.description, hash))
+        .collect();
+    let url_map: Vec<String> = versions.clone()
+        .iter()
+        .map(|version| format!("[\"{}\"]=\"{}\"", version.description, get_url(&version)))
+        .collect();
+    let region_map: Vec<String> = versions.clone().iter()
+        .map(|version| format!("[\"{}\"]=\"{}\"", version.description, version.region))
+        .collect();
+    let description_map: Vec<String> = versions.clone().iter()
+        .map(|version| version.description.to_owned())
+        .collect();
+    println!("declare -A HASHES=( {} )", hashes_map.join(" "));
+    println!("declare -A URLS=( {} )", url_map.join(" "));
+    println!("declare -A REGIONS=( {} )", region_map.join(" "));
+    println!("REGION_DESC=\"{}\"", description_map.join("|"));
+}
+
+fn download(http_client: &Client, download_url: &String, path: &Path) -> bool {
+    let result = http_client.get(download_url);
+    let mut result = result.send().expect("Failed to download file");
+    if result.status != StatusCode::Ok {
+        return false;
+    }
+    let mut out_file = File::create(path).expect("Could not create file.");
+    io::copy(&mut result, &mut out_file).expect("Failed to write to file.");
+    true
+}
+
+fn get_url(version: &LoLVersion) -> String {
+    format!("https://riotgamespatcher-a.akamaihd.net/ShellInstaller/{region}/LeagueofLegends_{region}_Installer_{version}.exe",
+            region = version.region, version = version.version)
+}
--- league_of_legends.sh
+++ league_of_legends.sh
@@ -8,14 +8,19 @@ WINE_STAGING_VERSION="1.9.23-staging"
 WINE_STABLE_VERSION="1.9.23"
 LEAGUE_OF_LEGENDS_VERSION="2016_11_10"
 
+declare -A HASHES=( ["Russia"]="7803b38f78badc03585fb0ef588d130b" ["EU Nordic & East"]="ca65a9d529310603b2777d703e490dbc" ["North America"]="aad5fb3163e32a77a48f65fda820275c" ["Latin America South"]="8750db743cc19c50eee4454d21f1ce60" ["Latin America North"]="e2126127aa73b497a2d12b2ff17316a5" ["Brazil"]="782c92b0f8f4920385a9d6782d0c7292" ["EU West"]="5ef1f0c65fb99296a03023f80a74d104" ["Oceania"]="761af34a3054db7661edfe3bf55c48ee" )
+declare -A URLS=( ["EU West"]="https://riotgamespatcher-a.akamaihd.net/ShellInstaller/EUW/LeagueofLegends_EUW_Installer_2016_11_10.exe" ["North America"]="https://riotgamespatcher-a.akamaihd.net/ShellInstaller/NA/LeagueofLegends_NA_Installer_2016_05_13.exe" ["EU Nordic & East"]="https://riotgamespatcher-a.akamaihd.net/ShellInstaller/EUNE/LeagueofLegends_EUNE_Installer_2016_11_10.exe" ["Oceania"]="https://riotgamespatcher-a.akamaihd.net/ShellInstaller/OC1/LeagueofLegends_OC1_Installer_2016_05_13.exe" ["Russia"]="https://riotgamespatcher-a.akamaihd.net/ShellInstaller/RU/LeagueofLegends_RU_Installer_2016_05_13.exe" ["Latin America North"]="https://riotgamespatcher-a.akamaihd.net/ShellInstaller/LA1/LeagueofLegends_LA1_Installer_2016_05_26.exe" ["Latin America South"]="https://riotgamespatcher-a.akamaihd.net/ShellInstaller/LA2/LeagueofLegends_LA2_Installer_2016_05_27.exe" ["Brazil"]="https://riotgamespatcher-a.akamaihd.net/ShellInstaller/BR/LeagueofLegends_BR_Installer_2016_05_13.exe" )
+declare -A REGIONS=( ["EU West"]="EUW" ["North America"]="NA" ["EU Nordic & East"]="EUNE" ["Oceania"]="OC1" ["Russia"]="RU" ["Latin America North"]="LA1" ["Latin America South"]="LA2" ["Brazil"]="BR" )
+REGION_DESC="EU West|North America|EU Nordic & East|Oceania|Russia|Latin America North|Latin America South|Brazil"
+
 POL_SetupWindow_Init
 
 POL_SetupWindow_message "Welcome to this wizard for installing League of Legends with the newer installers and unpatched wine $(pwd)"
 
-POL_SetupWindow_menu "Select your region" "Select region" "NA|EUW|EUNE|OC1|RU|LA1" "|"
-REGION="$APP_ANSWER"
-
-LOL_DOWNLOAD_URL="https://riotgamespatcher-a.akamaihd.net/ShellInstaller/$REGION/LeagueofLegends_${REGION}_Installer_$LEAGUE_OF_LEGENDS_VERSION.exe"
+POL_SetupWindow_menu "Select your region" "Select region" "$REGION_DESC" "|"
+REGION="${REGIONS[$APP_ANSWER]}"
+LOL_DOWNLOAD_HASH="${HASHES[$APP_ANSWER]}"
+LOL_DOWNLOAD_URL="${URLS[$APP_ANSWER]}"
 
 POL_SetupWindow_message "Going to download $LOL_DOWNLOAD_URL"
 
@@ -34,18 +39,16 @@ POL_System_TmpCreate "lol_msi"
 cd "$POL_System_TmpDir"
 EXE="$(basename $LOL_DOWNLOAD_URL)"
 INSTALLER="$POL_System_TmpDir/$EXE"
-POL_Download "$LOL_DOWNLOAD_URL" "5ef1f0c65fb99296a03023f80a74d104" "$POL_System_TmpDir"
+POL_Download "$LOL_DOWNLOAD_URL" "$LOL_DOWNLOAD_HASH" "$POL_System_TmpDir"
 
 POL_Wine "$INSTALLER" "/extract:$(winepath -w $POL_System_TmpDir/lol_msi)" "/execnoui"
-INSTALL_PATH="$(winepath -w '$PREFIX/drive_c/Riot Games')"
-POL_SetupWindow_message "$INSTALL_PATH"
-POL_Wine "msiexec" "/i" "$(winepath -w $POL_System_TmpDir/LoL.$REGION.msi)" "APPDIR=C:\\Riot Games" "/q"
-#POL_Wine "msiexec" "/i" "$(winepath -w $POL_System_TmpDir/LoL.$REGION.msi)" "APPDIR=\"$INSTALL_PATH\"" "/q"
+POL_Wine "msiexec" "/i" "$POL_System_TmpDir/LoL.$REGION.msi" "APPDIR=C:\\Riot Games" "/q"
 
 POL_Call POL_Install_vcrun2005
 POL_Call POL_Install_d3dx9
 
 Set_OS "winxp"
+POL_Call POL_Function_OverrideDLL builtin,native wininet
 
 POL_Shortcut "lol.launcher.admin.exe" "League of Legends"
 
