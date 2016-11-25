7385ad643e9f561408382e85cfd60c75ce891e83
Scetch <lucierbrandon@gmail.com>
2016-11-25 18:00:40 -0500

    Clean up things and use less allocations.

--- collect_hashes/src/main.rs
+++ collect_hashes/src/main.rs
@@ -1,130 +1,96 @@
 extern crate crypto;
 extern crate reqwest;
 
-use std::io::{self, Read, Write};
+use std::fs::{self, File};
+use std::io::prelude::*;
 use std::path::Path;
-use std::fs;
-use std::fs::File;
 
 use crypto::md5::Md5;
 use crypto::digest::Digest;
 
-use reqwest::Client;
-use reqwest::StatusCode;
+use reqwest::{Client, StatusCode};
 
-#[derive(Hash, Eq, PartialEq)]
+#[derive(Debug)]
 struct LoLVersion {
     region: &'static str,
     version: &'static str,
-    description: &'static str
+    desc: &'static str,
 }
 
-static VERSIONS: [LoLVersion; 8] = [
-    LoLVersion {
-        version: "2016_11_10",
-        region: "EUW",
-        description: "EU West"
-    },
-    LoLVersion {
-        region: "NA",
-        version: "2016_05_13",
-        description: "North America"
-    },
-    LoLVersion {
-        region: "EUNE",
-        version: "2016_11_10",
-        description: "EU Nordic & East"
-    },
-    LoLVersion {
-        region: "OC1",
-        version: "2016_05_13",
-        description: "Oceania"
-    },
-    LoLVersion {
-        region: "RU",
-        version: "2016_05_13",
-        description: "Russia"
-    },
-    LoLVersion {
-        region: "LA1",
-        version: "2016_05_26",
-        description: "Latin America North"
-    },
-    LoLVersion {
-        region: "LA2",
-        version: "2016_05_27",
-        description: "Latin America South"
-    },
-    LoLVersion {
-        region: "BR",
-        version: "2016_05_13",
-        description: "Brazil"
-    }
+static VERSIONS: &'static [LoLVersion] = &[
+    LoLVersion { region: "EUW", version: "2016_11_10", desc: "EU West" },
+    LoLVersion { region: "NA", version: "2016_05_13", desc: "North America" },
+    LoLVersion { region: "EUNE", version: "2016_11_10", desc: "EU Nordic & East" },
+    LoLVersion { region: "OC1", version: "2016_05_13", desc: "Oceania" },
+    LoLVersion { region: "RU", version: "2016_05_13", desc: "Russia" },
+    LoLVersion { region: "LA1", version: "2016_05_26", desc: "Latin America North" },
+    LoLVersion { region: "LA2", version: "2016_05_27", desc: "Latin America South" },
+    LoLVersion { region: "BR", version: "2016_05_13", desc: "Brazil" },
 ];
 
 fn main() {
-    let http_client = Client::new().expect("Couldn't create client");
-    let mut regions_found = vec!();
-    let mut hash = Md5::new();
-    let cache_folder = Path::new("filecache");
-    if !cache_folder.exists() {
-        fs::create_dir(cache_folder).expect("Failed to create installer cache folder");
+    let cache_path = Path::new("filecache");
+
+    if !cache_path.exists() {
+        fs::create_dir(cache_path).expect("Could not create cache directory");
     }
 
+    let mut found = Vec::new();
+    let mut hash = Md5::new();
+    let http_client = Client::new().expect("Could not create http client");
 
-    for version in &VERSIONS {
-        let download_url = get_url(&version);
-        println!("Calculating hash for url: {}", download_url);
-        // Check if file exists in cache
-        let executable = cache_folder.join(format!("LeagueofLegends_{}_Installer_{}.exe", version.region, version.version));
-        if !executable.exists() {
-            if !download(&http_client, &download_url, &executable) {
-                println!("Warning: {} returned 404", download_url);
+    for v in VERSIONS {
+        let url = format!("https://riotgamespatcher-a.akamaihd.net/ShellInstaller/{region}/LeagueofLegends_{region}_Installer_{version}.exe",
+            region = v.region, version = v.version);
+
+        println!("Calculating hash for url: {}", url);
+
+        let exec = cache_path.join(format!("LeagueofLegends_{}_Installer_{}.exe", v.region, v.version));
+
+        // If the installer doesn't exist try to download it.
+        if !exec.exists() {
+            let mut res = http_client.get(&url).send().expect("Could not download file");
+
+            if *res.status() != StatusCode::Ok {
+                println!("Warning: Couldn't download file {}", url);
                 continue;
             }
+
+            let mut buf = vec![];
+            res.read_to_end(&mut buf).expect("Could not read from response");
+
+            let mut file = File::create(&exec).expect(&format!("Could not create installer at: {:?}", exec));
+            file.write_all(&buf).expect("Could not write to file {}");
         }
 
-        let mut file = File::open(&executable).expect(format!("Could not find installer: {:?}", executable).as_ref());
+        let mut file = File::open(&exec).expect(&format!("Could not find installer: {:?}", exec));
 
+        let mut buf = vec![];
+        file.read_to_end(&mut buf).expect("Could not read installer data");
+        hash.input(&buf);
 
-        let mut bytes = vec!();
-        file.read_to_end(&mut bytes).expect("Failed to read remote content");
-        hash.input(&bytes);
-        let hash_result = hash.result_str();
-        println!("{}", &hash_result);
-        regions_found.push((version, hash_result));
+        let hash_str = hash.result_str();
+        println!("{}", hash_str);
+
         hash.reset();
+
+        found.push((hash_str, url, v));
     }
 
-    let hashes_map: Vec<String> = regions_found.iter()
-        .map(|&(ref version, ref hash)| format!("[\"{}\"]=\"{}\"", version.description, hash))
-        .collect();
-    let url_map: Vec<String> = regions_found.iter()
-        .map(|&(ref version, _)| format!("[\"{}\"]=\"{}\"", version.description, get_url(&version)))
-        .collect();
-    let region_map: Vec<String> = regions_found.iter()
-        .map(|&(ref version, _)| format!("[\"{}\"]=\"{}\"", version.description, version.region))
-        .collect();
-    let description_map: Vec<String> = regions_found.iter()
-        .map(|&(ref version, _)| version.description.to_owned())
-        .collect();
-    println!("declare -A HASHES=( {} )", hashes_map.join(" "));
-    println!("declare -A URLS=( {} )", url_map.join(" "));
-    println!("declare -A REGIONS=( {} )", region_map.join(" "));
-    println!("REGION_DESC=\"{}\"", description_map.join("|"));
-}
+    // Create string outputs for hashes, urls, regions, and descriptions using 4 string allocations.
+    let (mut hashes, mut urls, mut regions, mut desc) = found.iter()
+        .fold((String::new(), String::new(), String::new(), String::new()), |s, &(ref hash, ref url, v)| {
+            ((s.0 + "[\"" + v.desc + "\"]=\"" + hash + "\" "),
+             (s.1 + "[\"" + v.desc + "\"]=\"" + url + "\" "),
+             (s.2 + "[\"" + v.desc + "\"]=\"" + v.region + "\" "),
+             (s.3 + v.desc + "|"))
+        });
 
-fn download(http_client: &Client, download_url: &String, path: &Path) -> bool {
-    let mut result = http_client.get(download_url).send().expect("Failed to download file");
-    if *(result.status()) != StatusCode::Ok {
-        return false;
-    }
-    let mut out_file = File::create(path).expect("Could not create file.");
-    io::copy(&mut result, &mut out_file).expect("Failed to write to file.");
-    true
-}
+    // For now we just pop the extra last character off of each string because of the fold.
+    hashes.pop(); urls.pop(); regions.pop(); desc.pop();
 
-fn get_url(version: &LoLVersion) -> String {
-    format!("https://riotgamespatcher-a.akamaihd.net/ShellInstaller/{region}/LeagueofLegends_{region}_Installer_{version}.exe",
-            region = version.region, version = version.version)
+    println!("declare -A HASHES=( {} )", hashes);
+    println!("declare -A URLS=( {} )", urls);
+    println!("declare -A REGIONS=( {} )", regions);
+    println!("REGION_DESC=\"{}\"", desc);
 }
